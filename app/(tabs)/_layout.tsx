f0f0ac3730007c995e5ed1b4baffabd68ee95b1d
Andrei Bogdan <31503124+andreibt@users.noreply.github.com>
2025-11-11 17:39:18 +0200

    Build attendance management flows

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,15 +19,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Teacher',
+          tabBarIcon: ({ color }) => <IconSymbol size={26} name="person.crop.rectangle" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Student',
+          tabBarIcon: ({ color }) => <IconSymbol size={26} name="person.2.fill" color={color} />,
         }}
       />
     </Tabs>
